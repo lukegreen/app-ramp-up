f32742fd6c71b52a59647401158fc1c1774de726
Luke Green <elukuent@gmail.com>
2016-07-27 15:40:45 -0700

    generated angular framework, ran set up and config
    set up node-sass, grunt buildcontrol, etc.

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name appRampUpApp
+ * @description
+ * # appRampUpApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('appRampUpApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appRampUpApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the appRampUpApp
+ */
+angular.module('appRampUpApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
