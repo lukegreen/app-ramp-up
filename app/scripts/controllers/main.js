027351c825b0a4b5d1575a24373369c5bc88978e
Luke Green <elukuent@gmail.com>
2016-07-28 16:37:25 -0700

    update
    update

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,4 +10,10 @@
 angular.module('appRampUpApp')
   .controller('MainCtrl', function ($scope, current) {
     $scope.current = current.query();
+
+    $scope.refreshCurrent = function(){
+       $scope.current = current.query({
+           location: $scope.location
+       });
+     };
   });
